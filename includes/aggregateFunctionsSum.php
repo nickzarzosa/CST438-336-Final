f2b13c2308c3165ee2936a52e8cc69af329149b4
lamont92 <alamont@csumb.edu>
2015-05-06 21:17:37 +0000

    fixed includes/aggregateFunctions.php and added includes/aggregateFunctionsSum.php

--- includes/aggregateFunctionsSum.php
+++ includes/aggregateFunctionsSum.php
@@ -3,25 +3,24 @@
 require 'dbConnectioin.php'; //require database connection
 $dbConn = getConnection(); //connects with database and tables
 
-$sql = "SELECT firstname, lastname, age, gender FROM users WHERE gender = 'M'"; //sql statement to fetch all users who are male
+$sql = "SELECT COUNT(gender)FROM users WHERE gender = 'M'"; //aggregate function to count the number of males  
 $namedParameters = array();
 
 $stmt = $dbConn->prepare($sql);
 $stmt->execute($namedParameters);
 $result = $stmt->fetch();
 
- echo "Printing users who are male" . print_r($result);
-
-$sql = "SELECT firstname, lastname, age, gender FROM users WHERE gender = 'F'"; //sql statement to fetch all users who are female
-$namedParameters = array();
-
-$stmt = $dbConn->prepare($sql);
-$stmt->execute($namedParameters);
-$result = $stmt->fetch();
-
-echo "Printing users who are female" 
+echo "Printing users who are male" 
 print_r($result);
 
+$sql = "SELECT COUNT(gender)FROM users WHERE gender = 'F'"; //aggregate function to count the number of females  
+$namedParameters = array();
 
+$stmt = $dbConn->prepare($sql);
+$stmt->execute($namedParameters);
+$result = $stmt->fetch();
+
+echo "Printing users who are male" 
+print_r($result);
 
 ?>
